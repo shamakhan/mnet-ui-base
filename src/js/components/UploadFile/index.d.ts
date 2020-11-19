3208b62748a9e5a82067692de37932c2802f8329
amit.mish <amitmishra009v@gmail.com>
2020-11-19 16:20:42 +0530

    [UploadFileProps added]

--- src/js/components/UploadFile/index.d.ts
+++ src/js/components/UploadFile/index.d.ts
@@ -1,16 +1,10 @@
 import * as React from "react";
-import {
-  PolymorphicType
-} from "../../utils";
-
-export interface PaginationProps {
-  leftIcon?: PolymorphicType;
-  rightIcon?: PolymorphicType;
-  pages?: number;
-  currentPage?: number;
-  onClick?: (value) => void;
+export interface UploadFileProps {
+  validateUrl?: string;
+  uploadUrl?: string;
+  downloadSampleUrl?: string;
 }
 
-declare const Pagination: React.FC<PaginationProps>;
+declare const UploadFile: React.FC<UploadFileProps>;
 
-export { Pagination };
+export { UploadFile };
