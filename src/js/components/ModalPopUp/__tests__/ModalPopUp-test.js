7456b4de52e889fde7a765292c9832007c5f4703
saumik.s <saumik.s@media.net>
2020-12-17 17:41:08 +0530

    [Unit Test Cases] ModalPopUp Component test cases

--- src/js/components/ModalPopUp/__tests__/ModalPopUp-test.js
+++ src/js/components/ModalPopUp/__tests__/ModalPopUp-test.js
@@ -1,16 +1,27 @@
 import React from 'react';
 import 'jest-styled-components';
-import { cleanup, render, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
+import {
+  cleanup,
+  render,
+  fireEvent,
+  waitForElementToBeRemoved,
+} from '@testing-library/react';
 
 import { createPortal } from '../../../utils/portal';
-import { ModalPopUp, openConfirmAlert } from '../ModalPopUp';
+import {
+  closeConfirmAlert,
+  modalIsLoading,
+  ModalPopUp,
+  openConfirmAlert,
+} from '../ModalPopUp';
 
 describe('ModalPopUp', () => {
   const openButtonText = 'TEST OPEN MODAL';
   const title = 'TEST MODAL';
   const message = 'TEST MESSAGE CONTENT';
   const okLabel = 'OK';
+  const cancelLabel = 'Cancel';
 
   const clicks = {
     okButtonClick: jest.fn(),
@@ -23,7 +34,9 @@ describe('ModalPopUp', () => {
      * 
      * 0 - basic
      * 1 - without title
-     * 2 - custom 
+     * 2 - custom message
+     * 3 - custom body
+     * 4 - without onPrimaryClick
      * 
      */
 
@@ -45,7 +58,15 @@ describe('ModalPopUp', () => {
                         {okLabel}
                       </button>,
       }
-    }
+      case 3: return {
+        title: <span>{title}</span>,
+        body: <div><span>{message}</span></div>,
+        renderButton: <button type="button" onClick={clicks.okButtonClick}>
+                        {okLabel}
+                      </button>,
+      }
+      case 4: return { title, message }
+    };
 
     return null;
     
@@ -118,7 +139,57 @@ describe('ModalPopUp', () => {
     expect(baseElement).toMatchSnapshot();
   });
 
+  it('Custom render elements - custom body', () => {
+    const { baseElement, getByText } = render(
+      <>
+        <ModalPopUp id="test-modal-pop-up" />
+        <button
+          type="button"
+          onClick={() => openConfirmAlert(getPopUpPayload(3))}
+        >
+          {openButtonText}
+        </button>
+      </>,
+    );
+
+    fireEvent.click(getByText(openButtonText));
+
+    expect(baseElement).toMatchSnapshot();
+  });
+
   /* Unit Test Cases */
+
+  it('Pop-up open and close', () => {
+    const { getByText, queryByRole } = render(
+      <>
+        <ModalPopUp id="test-modal-pop-up" />
+        <button
+          type="button"
+          onClick={() => openConfirmAlert(getPopUpPayload(0))}
+        >
+          {openButtonText}
+        </button>
+        <button
+          type="button"
+          onClick={closeConfirmAlert}
+        >
+          {cancelLabel}_modal_close
+        </button>
+      </>,
+    );
+    
+    expect(queryByRole('dialog', { name: 'modal-pop-up' })).toBeNull();
+
+    fireEvent.click(getByText(openButtonText));
+    expect(queryByRole('dialog', { name: 'modal-pop-up' })).toBeInTheDocument();
+
+    fireEvent.click(getByText(`${cancelLabel}_modal_close`));
+    waitForElementToBeRemoved(() => queryByRole(
+      'dialog', { name: 'modal-pop-up' },
+    )).then(() => {
+      expect(queryByRole('dialog', { name: 'modal-pop-up' })).toBeNull();
+    });
+  });
 
   it('Basic - OK Click', () => {
     const { getByText } = render(
@@ -156,6 +227,87 @@ describe('ModalPopUp', () => {
     fireEvent.click(getByText(okLabel));
 
     expect(okButtonClickSpy).toHaveBeenCalledTimes(1);
+  });
+
+  it('Basic - OK Click - without onPrimaryClick option', () => {
+    const { getByText, queryByRole } = render(
+      <>
+        <ModalPopUp id="test-modal-pop-up" />
+        <button
+          type="button"
+          onClick={() => openConfirmAlert(getPopUpPayload(4))}
+        >
+          {openButtonText}
+        </button>
+      </>,
+    );
+
+    fireEvent.click(getByText(openButtonText));
+    fireEvent.click(getByText(okLabel));
+
+    waitForElementToBeRemoved(() => queryByRole(
+      'dialog', { name: 'modal-pop-up' },
+    )).then(() => {
+      expect(queryByRole('dialog', { name: 'modal-pop-up' })).toBeNull();
+    });
+  });
+  
+  it('Cancel click', () => {
+    const { getByText, queryByRole } = render(
+      <>
+        <ModalPopUp id="test-modal-pop-up" />
+        <button
+          type="button"
+          onClick={() => openConfirmAlert(getPopUpPayload(0))}
+        >
+          {openButtonText}
+        </button>
+      </>,
+    );
+
+    fireEvent.click(getByText(openButtonText));
+    fireEvent.click(getByText(cancelLabel));
+
+    waitForElementToBeRemoved(() => queryByRole(
+      'dialog', { name: 'modal-pop-up' },
+    )).then(() => {
+      expect(queryByRole('dialog', { name: 'modal-pop-up' })).toBeNull();
+    });
+  });
+
+  it('Loading state', () => {
+    const { getByText, queryByRole } = render(
+      <>
+        <ModalPopUp id="test-modal-pop-up" />
+        <button
+          type="button"
+          onClick={() => openConfirmAlert(getPopUpPayload(0))}
+        >
+          {openButtonText}
+        </button>
+        <button
+          type="button"
+          onClick={() => modalIsLoading(true)}
+        >
+          SET_LOADER
+        </button>
+        <button
+          type="button"
+          onClick={() => modalIsLoading(false)}
+        >
+          RESET_LOADER
+        </button>
+      </>,
+    );
+
+    fireEvent.click(getByText(openButtonText));
+    expect(queryByRole('alert', { name: 'button-loader' })).toBeNull();
+
+    fireEvent.click(getByText('SET_LOADER'));
+    expect(queryByRole('alert', { name: 'button-loader' })).toBeInTheDocument();
+
+    fireEvent.click(getByText('RESET_LOADER'));
+    expect(queryByRole('alert', { name: 'button-loader' })).toBeNull();
   });
 
 });
